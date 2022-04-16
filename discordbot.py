92f09dcbaa3fd3312eaf8590329011f87c415003
Shadblade5 <shadblade8@gmail.com>
2022-04-15 21:24:25 -0400

    New Functionalities Added

--- discordbot.py
+++ discordbot.py
@@ -13,8 +13,8 @@ intents.members = True
 bot = commands.Bot(command_prefix=config.c.prefix, description=description, intents=intents)
 
 DB = database.DBClient(config.c.db_host,config.c.db_username,config.c.db_password)
-userinfo = DB.getUsers()
-print(userinfo[0])
+#userinfo = DB.getUsers()
+#print(userinfo[0])
 
 @bot.event
 async def on_ready():
@@ -23,11 +23,37 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.event
+async def on_member_join(ctx):
+    DB.adduser(ctx.id,ctx.name)
+
+@bot.command()
+async def getusers(ctx):
+    await ctx.send(DB.getUsers())
+
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
+@bot.command()
+async def getUserInfo(ctx):
+    userinfo = DB.getUserInfo(ctx.author.id)
+    await ctx.send(userinfo)
+
+@bot.command()
+async def addUser(ctx):
+    DB.adduser(ctx.author.id,ctx.author.name)
+
+@bot.command()
+async def setBirthday(ctx,month,day,year):
+    birthday = "{0}-{1}-{2}".format(year,month,day)
+    #await ctx.send(birthday)
+    DB.setBirthday(ctx.author.id,birthday)
+
+@bot.command()
+async def getBalance(ctx):
+    await ctx.send(DB.getBalance(ctx.author.id))
 
 
 
